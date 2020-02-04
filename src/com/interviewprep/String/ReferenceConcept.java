a525f9d122edf536078d53d56b2f0e5f866232a3
mohit <moh.hit3@gmail.com>
2020-02-04 09:42:50 +0530

    Testing push on github 2

--- src/com/interviewprep/String/ReferenceConcept.java
+++ src/com/interviewprep/String/ReferenceConcept.java
@@ -3,6 +3,7 @@ package com.interviewprep.String;
 public class ReferenceConcept {
 
     public void checkRefConcept(Name name){
+        System.out.println("Inside checkRefConcept");
         name.setName("XXXX");
 
         name =null;
@@ -12,9 +13,12 @@ public class ReferenceConcept {
         Person p = new Person();
         p.setName(new Name("Mohit"));
 
+
         ReferenceConcept ref = new ReferenceConcept();
         ref.checkRefConcept(p.getName());
 
         System.out.println(">> "+p.getName().getName());
+
+        System.out.println("Exit !!!");
     }
 }
--- src/com/multithreading/Main.java
+++ src/com/multithreading/Main.java
@@ -1,4 +1,9 @@
 package com.multithreading;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("In Main");
+    }
 }
